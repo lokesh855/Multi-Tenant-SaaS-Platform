2ca9fc977b5240e9e94a40489ed39774ea476398
lokesh855 <119839757+lokesh855@users.noreply.github.com>
2026-01-03 11:28:34 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export const API = axios.create({
 
-  baseURL: "https://backend-production-ff56.up.railway.app/api" || import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:5000/api" || import.meta.env.VITE_API_URL,
 
 });
 
